eb1530741bbf87eeb2dcf0208beb5d2e2548fdb9
ihleven <github@ihleven.de>
2020-07-19 11:27:00 +0200

    Fehlerseite mit Navigation, _ausgabe.vue ohne bootstrap

--- src/pages/geheimtipp/_ausgabe.vue
+++ src/pages/geheimtipp/_ausgabe.vue
@@ -1,55 +1,36 @@
 <template>
     <div class="edition-wrapper">
-        <!--<h1 class="text-hide" style="background-image: url('/assets/logos/logo-WM2014.svg'); width: 500px; height: 500px;">Bootstrap</h1>-->
-
-        <nav class="navbar navbar-expand-sm navbar-light root sticky-top">
-            <n-link class="navbar-brand" :to="'/' + ausgabe.code">
+        <nav class="ilvn-navbar">
+            <n-link class="logo" :to="'/' + ausgabe.code">
                 <img v-if="ausgabe.type == 'L'" src="~assets/logos/bl/bl-logo-quer.svg" style="height: 2rem;" />
                 <img v-if="ausgabe.type == 'T'" src="~assets/logos/world-cup-2018.png" style="height: 50px;" />
             </n-link>
+            <ul v-if="ausgabe.type == 'L'" class="ilvn-nav">
+                <li v-for="compseason in compseasons" :key="compseason">
+                    <n-link class="ilvn-nav-item yanone" :to="{ path: 'BL' + compseason }" append router-link-active="active">{{ compseason }}. Liga</n-link>
+                </li>
+                <li>
+                    <n-link class="ilvn-nav-item yanone" to="klassement" router-link-active="active">Auswertung</n-link>
+                </li>
+                <li>
+                    <n-link v-if="!ausgabe.registration" class="ilvn-nav-item yanone" to="register">Hier bitte zuerst registrieren</n-link>
+                </li>
+            </ul>
 
-            <button
-                class="navbar-toggler"
-                type="button"
-                data-toggle="collapse"
-                data-target="#navbarNav"
-                aria-controls="navbarNav"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-            >
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div id="navbarNav" class="collapse navbar-collapse">
-                <ul v-if="ausgabe.type == 'L'" class="navbar nav">
-                    <li v-for="compseason in compseasons" :key="compseason" class="nav-item">
-                        <n-link class="nav-link yanone menu-link" :to="{ path: 'BL' + compseason }" append router-link-active="active"
-                            >{{ compseason }}. Liga</n-link
-                        >
-                    </li>
-                    <li class="nav-item">
-                        <n-link class="nav-link yanone menu-link" to="klassement" router-link-active="active">Auswertung</n-link>
-                    </li>
-                    <li class="nav-item">
-                        <n-link v-if="!ausgabe.registration" class="nav-link yanone menu-link red" to="register">Hier bitte zuerst registrieren</n-link>
-                    </li>
-                </ul>
-
-                <ul v-if="ausgabe.type == 'T'" class="navbar-nav">
-                    <li class="nav-item" routerLinkActive="active">
-                        <n-link class="nav-link yanone menu-link" to="spiele" router-link-active="active">Spiele</n-link>
-                    </li>
-                    <li class="nav-item">
-                        <n-link class="nav-link yanone menu-link" to="groups" router-link-active="active">Gruppen</n-link>
-                    </li>
-                    <li class="nav-item" routerLinkActive="active">
-                        <n-link class="nav-link yanone menu-link" to="klassement" router-link-active="active">Ranking</n-link>
-                    </li>
-                    <li v-if="!ausgabe.registration" class="nav-item" routerLinkActive="active">
-                        <n-link class="nav-link yanone menu-link" to="register" router-link-active="active">Registrierung</n-link>
-                    </li>
-                </ul>
-            </div>
+            <ul v-if="ausgabe.type == 'T'" class="ilvn-nav">
+                <li>
+                    <n-link class="ilvn-nav-item yanone" to="spiele" router-link-active="active">Spiele</n-link>
+                </li>
+                <li>
+                    <n-link class="ilvn-nav-item yanone" to="groups" router-link-active="active">Gruppen</n-link>
+                </li>
+                <li>
+                    <n-link class="ilvn-nav-item yanone" to="klassement" router-link-active="active">Ranking</n-link>
+                </li>
+                <li v-if="!ausgabe.registration">
+                    <n-link class="ilvn-nav-item yanone" to="register" router-link-active="active">Registrierung</n-link>
+                </li>
+            </ul>
         </nav>
 
         <section class="content">
@@ -90,31 +71,49 @@ export default {
     },
 }
 </script>
-
-<style scoped>
-.root {
-    padding-top: 0;
-    padding-bottom: 0;
-    border: 0px solid green;
-    background-color: white;
-    background: #e8e8e8;
-    box-shadow: 2px 4px 5px 0 rgba(0, 0, 0, 0.2);
-}
-
+<style lang="css" scoped>
 .edition-wrapper {
     border: 1px dotted red;
 }
+.ilvn-navbar {
+    position: sticky;
+    top: 0;
 
-.menu-link {
-    font-size: 200%;
-    font-wight: bold;
-    padding-top: 0;
-    padding-bottom: 0;
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: flex-start;
+    padding: 0 1rem;
+
+    background: #e8e8e8;
+    box-shadow: 2px 4px 5px 0 rgba(0, 0, 0, 0.2);
 }
+.ilvn-navbar .logo {
+    display: inline-block;
+    padding-top: 0.3125rem;
+    padding-bottom: 0.3125rem;
+    margin-right: 1rem;
+    font-size: 1.25rem;
+    line-height: inherit;
+    white-space: nowrap;
+}
+.ilvn-navbar .ilvn-nav {
+    position: relative;
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0;
+    margin: 0;
+    list-style: none;
+}
+
+.ilvn-navbar .ilvn-nav-item {
+    padding: 0.5rem 1rem;
+    font-size: 1.5rem;
+}
+
 section.content {
     background: white;
-}
-.red {
-    color: red;
 }
 </style>
